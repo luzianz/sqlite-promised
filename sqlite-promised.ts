adddfb622affc17966a07725da8d9e178afde263
Luzian Zagadinow <luzianz@outlook.com>
2015-06-07 05:41:52 -0400

    replace references to sqlite classes with interfaces

--- sqlite-promised.ts
+++ sqlite-promised.ts
@@ -1,19 +1,17 @@
 /// <reference path="typings/sqlite3/sqlite3.d.ts" />
-/// <reference path="node_modules/lz-tslib-interfaces/IPromise" />
+/// <reference path="IPromise" />
+/// <reference path="ISqliteDatabase" />
+/// <reference path="IStatementComposite" />
 
 //TODO: comment code
 
 import sqlite3 = require('sqlite3');
 var Promise = require('promiz');
 
-export interface IStatementComposite<T> {
-	statement: sqlite3.Statement;
-	passedValue: T;
-}
 
-export function openDatabase(filePath: string): IPromise<sqlite3.Database> {
-	return createPromise(function (resolve, reject) {
-		var db = new sqlite3.Database(filePath, function (error) {
+export function openDatabase(filePath: string): IPromise<ISqliteDatabase> {
+	return createPromise(function(resolve, reject) {
+		var db = new sqlite3.Database(filePath, function(error) {
 			if (error) {
 				reject(error);
 			} else {
@@ -23,9 +21,9 @@ export function openDatabase(filePath: string): IPromise<sqlite3.Database> {
 	});
 }
 
-export function closeDatabase<T>(db: sqlite3.Database, pass?: T): IPromise<T> {
-	return createPromise(function (resolve, reject) {
-		db.close(function (error) {
+export function closeDatabase<T>(db: ISqliteDatabase, pass?: T): IPromise<T> {
+	return createPromise(function(resolve, reject) {
+		db.close(function(error) {
 			if (error) {
 				reject(error);
 			} else {
@@ -35,9 +33,9 @@ export function closeDatabase<T>(db: sqlite3.Database, pass?: T): IPromise<T> {
 	});
 }
 
-export function prepareStatement(db: sqlite3.Database, sql: string): IPromise<sqlite3.Statement> {
-	return createPromise(function (resolve, reject) {
-		var stmt = db.prepare(sql, function (error) {
+export function prepareStatement(db: ISqliteDatabase, sql: string): IPromise<ISqliteStatement> {
+	return createPromise(function(resolve, reject) {
+		var stmt = db.prepare(sql, function(error) {
 			if (error) {
 				reject(error);
 			} else {
@@ -47,9 +45,9 @@ export function prepareStatement(db: sqlite3.Database, sql: string): IPromise<sq
 	});
 }
 
-export function finalizeStatement<T>(stmt: sqlite3.Statement, pass?: T): IPromise<T> {
-	return createPromise(function (resolve, reject) {
-		stmt.finalize(function (error) {
+export function finalizeStatement<T>(stmt: ISqliteStatement, pass?: T): IPromise<T> {
+	return createPromise(function(resolve, reject) {
+		stmt.finalize(function(error) {
 			if (error) {
 				reject(error);
 			} else {
@@ -59,9 +57,9 @@ export function finalizeStatement<T>(stmt: sqlite3.Statement, pass?: T): IPromis
 	});
 }
 
-export function runStatement(stmt: sqlite3.Statement, params?): IPromise<sqlite3.Statement> {
-	return createPromise(function (resolve, reject) {
-		stmt.run(params, function (error) {
+export function runStatement(stmt: ISqliteStatement, params?): IPromise<ISqliteStatement> {
+	return createPromise(function(resolve, reject) {
+		stmt.run(params, function(error) {
 			if (error) {
 				reject(error);
 			} else {
@@ -71,9 +69,9 @@ export function runStatement(stmt: sqlite3.Statement, params?): IPromise<sqlite3
 	});
 }
 
-export function queryStatement<T>(stmt: sqlite3.Statement, params?): IPromise<IStatementComposite<T[]>> {
-	return createPromise(function (resolve, reject) {
-		stmt.all(params, function (error, rows: any[]) {
+export function queryStatement<T>(stmt: ISqliteStatement, params?): IPromise<IStatementComposite<T[]>> {
+	return createPromise(function(resolve, reject) {
+		stmt.all(params, function(error, rows: any[]) {
 			if (error) {
 				reject(error);
 			} else {
@@ -84,30 +82,30 @@ export function queryStatement<T>(stmt: sqlite3.Statement, params?): IPromise<IS
 }
 
 // ----------------------------------------------------------------------------
-export function runAndFinalize<T>(db: sqlite3.Database, sql: string, params?, pass?: T): IPromise<T> {
-	return prepareStatement(db, sql).then(function (stmt) {
+export function runAndFinalize<T>(db: ISqliteDatabase, sql: string, params?, pass?: T): IPromise<T> {
+	return prepareStatement(db, sql).then(function(stmt) {
 		return runStatement(stmt, params);
-	}).then(function (stmt) {
+	}).then(function(stmt) {
 		return finalizeStatement<T>(stmt, pass);
 	});
 }
 
-export function runAndClose<T>(db: sqlite3.Database, sql: string, params?, pass?: T): IPromise<T> {
-	return runAndFinalize(db, sql, params).then(function () {
+export function runAndClose<T>(db: ISqliteDatabase, sql: string, params?, pass?: T): IPromise<T> {
+	return runAndFinalize(db, sql, params).then(function() {
 		return closeDatabase<T>(db, pass);
 	});
 }
 
-export function queryAndFinalize<T>(db: sqlite3.Database, sql: string, params?): IPromise<T[]> {
-	return prepareStatement(db, sql).then(function (stmt) {
+export function queryAndFinalize<T>(db: ISqliteDatabase, sql: string, params?): IPromise<T[]> {
+	return prepareStatement(db, sql).then(function(stmt) {
 		return queryStatement<T>(stmt, params);
-	}).then(function (result) {
+	}).then(function(result) {
 		return finalizeStatement(result.statement, result.passedValue);
 	});
 }
 
-export function queryAndClose<T>(db: sqlite3.Database, sql: string, params?): IPromise<T[]> {
-	return queryAndFinalize<T>(db, sql, params).then(function (rows) {
+export function queryAndClose<T>(db: ISqliteDatabase, sql: string, params?): IPromise<T[]> {
+	return queryAndFinalize<T>(db, sql, params).then(function(rows) {
 		return closeDatabase(db, rows);
 	});
 }
