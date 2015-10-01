beb7e18bc6d12a71dedb638a84534f2a55a68f19
Luzian Zagadinow <luzianz@outlook.com>
2015-10-01 18:22:26 -0400

    fixed bug: use types from sqlite3.d.ts

--- sqlite-promised.d.ts
+++ sqlite-promised.d.ts
@@ -1,18 +1,16 @@
-/// <reference path="typings/sqlite3/sqlite3" />
-/// <reference path="IPromise" />
-/// <reference path="ISqliteDatabase" />
-/// <reference path="ISqliteStatement" />
-/// <reference path="IStatementComposite" />
+/// <reference path="typings/sqlite3/sqlite3.d.ts" />
+/// <reference path="node_modules/lz-tslib-interfaces/IPromise.d.ts" />
 
 declare module 'sqlite-promised' {
-	export function openDatabase(filePath: string): IPromise<ISqliteDatabase>;
-	export function closeDatabase<T>(db: ISqliteDatabase, pass?: T): IPromise<T>;
-	export function prepareStatement(db: ISqliteDatabase, sql: string): IPromise<ISqliteStatement>;
-	export function finalizeStatement<T>(stmt: ISqliteStatement, pass?: T): IPromise<T>;
-	export function runStatement(stmt: ISqliteStatement, params?: any): IPromise<ISqliteStatement>;
-	export function queryStatement<T>(stmt: ISqliteStatement, params?: any): IPromise<IStatementComposite<T[]>>;
-	export function runAndFinalize<T>(db: ISqliteDatabase, sql: string, params?: any, pass?: T): IPromise<T>;
-	export function runAndClose<T>(db: ISqliteDatabase, sql: string, params?: any, pass?: T): IPromise<T>;
-	export function queryAndFinalize<T>(db: ISqliteDatabase, sql: string, params?: any): IPromise<T[]>;
-	export function queryAndClose<T>(db: ISqliteDatabase, sql: string, params?: any): IPromise<T[]>;
+	import sqlite3 = require('sqlite3');
+	export function openDatabase(filePath: string): IPromise<sqlite3.Database>;
+	export function closeDatabase<T>(db: sqlite3.Database, pass?: T): IPromise<T>;
+	export function prepareStatement(db: sqlite3.Database, sql: string): IPromise<sqlite3.Statement>;
+	export function finalizeStatement<T>(stmt: sqlite3.Statement, pass?: T): IPromise<T>;
+	export function runStatement(stmt: sqlite3.Statement, params?: any): IPromise<sqlite3.Statement>;
+	export function queryStatement<T>(stmt: sqlite3.Statement, params?: any): IPromise<{statement: sqlite3.Statement, rows: T[]}>;
+	export function runAndFinalize<T>(db: sqlite3.Database, sql: string, params?: any, pass?: T): IPromise<T>;
+	export function runAndClose<T>(db: sqlite3.Database, sql: string, params?: any, pass?: T): IPromise<T>;
+	export function queryAndFinalize<T>(db: sqlite3.Database, sql: string, params?: any): IPromise<T[]>;
+	export function queryAndClose<T>(db: sqlite3.Database, sql: string, params?: any): IPromise<T[]>;
 }
