78c2f4d5859f00857f4678334ff51eeab93c9e3f
Luzian Zagadinow <luzianz@outlook.com>
2015-10-02 05:15:38 -0400

    Change demo to use an observer

--- usage/main.js
+++ usage/main.js
@@ -1,11 +1,16 @@
-var sqlite_promised = require('sqlite-promised');
+var sqlite_promised = require('../js/sqlite-promised');
+
+var observer = {
+	next: function(value) {
+		console.log(value.content);
+	}, error: function(error) {
+		console.error(error);
+	}, done: function() {
+	}
+}
 
 sqlite_promised.openDatabase('database.db').then(function (db) {
-	return sqlite_promised.queryAndClose(db, 'select * from "main"."vw_notes"');
-}).then(function (rows) {
-	rows.forEach(function (row) {
-		console.log(row.content);
-	});
+	return sqlite_promised.observeQueryAndClose(db, 'select * from "main"."vw_notes"', observer);
 }, function (err) {
 	console.error(err);
 });
