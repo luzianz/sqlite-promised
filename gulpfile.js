bf2c0de48f0b371fd7be8f2deefe0e9911160cbc
Luzian Zagadinow <luzianz@outlook.com>
2019-09-25 14:57:29 -0400

    cleaned-up gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,10 @@
-var gulp = require('gulp');
-var ts = require('gulp-typescript');
+const gulp = require('gulp');
+const ts = require('gulp-typescript');
 
-var tsProject = ts.createProject(
-	'tsconfig.json',
-	{
-		declaration: true
-	}
-);
+const tsProject = ts.createProject('tsconfig.json');
 
 gulp.task('default', function() {
 	return tsProject.src()
 		.pipe(tsProject())
-		// destination is redundant: it is also specified in tsconfig.json
-		//TODO: figure out how to use the destination in tsconfig.json
-		.pipe(gulp.dest('dist')); 
+		.pipe(gulp.dest('dist'));
 });
-
